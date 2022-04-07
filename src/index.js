74a0f388e007a4a3abc839207bb94074c6a3a4d2
Opeyemi <opeadetola@gmail.com>
2022-04-07 13:48:14 -0400

    Added contact component and created button for Contact

--- src/index.js
+++ src/index.js
@@ -1,10 +1,45 @@
 import _ from "lodash";
 import loadMenu from "./menu";
+import loadHome from "./home";
+import loadContact from "./contact";
 
-function component() {
-  const element = document.getElementById("content");
-  element.textContent = "Welcome";
-  return element;
+function createNav() {
+  const nav = document.createElement("nav");
+  //home button
+  const homeButton = document.createElement("button");
+  homeButton.classList.add("button-nav");
+  homeButton.textContent = "Home";
+  homeButton.addEventListener("click", (e) => {
+    if (e.target.classList.contains("active")) return;
+    setActiveButton(homeButton);
+    loadHome();
+  });
+
+  //menu button
+  const menuButton = document.createElement("button");
+  menuButton.classList.add("button-nav");
+  menuButton.textContent = "Menu";
+  menuButton.addEventListener("click", (e) => {
+    if (e.target.classList.contains("active")) return;
+    setActiveButton(menuButton);
+    loadMenu();
+  });
+
+  //contact button
+  const contactButton = document.createElement("button");
+  contactButton.classList.add("button-nav");
+  contactButton.textContent = "Contact";
+  contactButton.addEventListener("click", (e) => {
+    if (e.target.classList.contains("active")) return;
+    setActiveButton(contactButton);
+    loadContact();
+  });
+
+  nav.appendChild(homeButton);
+  nav.appendChild(menuButton);
+  nav.appendChild(contactButton);
+
+  return nav;
 }
 
-document.body.appendChild(component());
+document.body.appendChild(createNav());
